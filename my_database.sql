133148b14b591a54fe9001200146768b65bcdb32
Andrii <semenukandrej574@gmailcom>
2024-11-29 18:54:38 +0100

    commit

--- my_database.sql
+++ my_database.sql
@@ -1,13 +1,17 @@
--- Створення бази даних (якщо вона ще не існує)
 CREATE DATABASE IF NOT EXISTS my_database;
 
--- Перехід в базу даних
 USE my_database;
 
--- Створення таблиці users
 CREATE TABLE IF NOT EXISTS users (
                                      id INT AUTO_INCREMENT PRIMARY KEY, -- Цілочисельне поле, автоінкремент, первинний ключ
                                      name VARCHAR(255), -- Рядковий тип для зберігання імені
-    age INT, -- Цілочисельне поле для віку
-    email VARCHAR(255) -- Рядковий тип для зберігання email
-    );
+                                     age INT, -- Цілочисельне поле для віку
+                                     email VARCHAR(255) -- Рядковий тип для зберігання email
+);
+INSERT INTO users (name, age, email) VALUES
+                                         ('John', 30, 'john@example.com'),
+                                         ('Alice', 25, 'alice@example.com'),
+                                         ('Bob', 35, 'bob@example.com');
+SELECT * FROM users;
+DELETE FROM users WHERE name = 'Bob';
+SELECT * FROM users;
